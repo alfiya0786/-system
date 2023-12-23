6c03d2d68e1e59065f6a9e32191934bb3d22efff
alfiya0786 <146177595+alfiya0786@users.noreply.github.com>
2023-12-23 11:41:39 +0530

    Add files via upload

---
+++ t.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class t
+{
+	public static void main(String[] arg)
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.print("enter a:");
+		int a=in.nextInt();
+		System.out.print("a="+a);
+	}
+}
